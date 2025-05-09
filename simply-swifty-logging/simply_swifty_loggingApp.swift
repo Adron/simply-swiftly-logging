60dffb3e92a02c71cb6d886d149bdd5027d1678a
Adron Hall <adronhall@gmail.com>
2025-05-09 14:13:01 -0700

    Initial Commit

---
+++ simply-swifty-logging/simply_swifty_loggingApp.swift
@@ -0,0 +1,17 @@
+//
+//  simply_swifty_loggingApp.swift
+//  simply-swifty-logging
+//
+//  Created by Adron Hall on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct simply_swifty_loggingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ simply-swifty-loggingTests/simply_swifty_loggingTests.swift
@@ -0,0 +1,17 @@
+//
+//  simply_swifty_loggingTests.swift
+//  simply-swifty-loggingTests
+//
+//  Created by Adron Hall on 5/9/25.
+//
+
+import Testing
+@testable import simply_swifty_logging
+
+struct simply_swifty_loggingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
